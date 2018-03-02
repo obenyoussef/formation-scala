6bbe4ec16c750d2c11c1ac9c92db0da692962471
implicitdef <implicitdef@users.noreply.github.com>
2018-03-02 17:28:25 +0100

    Revert "version sans implementations"
    This reverts commit 9f858f9240a7f062cdfef5ef786f55a18a1cb6ed.

--- exo_bataille_fermee/src/main/scala/bataille/BatailleFermee.scala
+++ exo_bataille_fermee/src/main/scala/bataille/BatailleFermee.scala
@@ -14,20 +14,35 @@ class BatailleFermee {
   // joue tous les plis jusqu'à ce qu'un des joueurs a gagné toutes les cartes
   // retourne :
   // - (le joueur qui a gagné, combien de plus étaient nécessaire)
-  def joue(joueurs: Seq[Joueur]): (Joueur, Int) = ???
+  def joue(joueurs: Seq[Joueur]): (Joueur, Int) = {
+    val etatInitial = construitEtatInitial(joueurs)
+    val etatFinal = jouePlisJusquaVictoire(etatInitial)
+    (etatFinal.tasParJoueur.keys.head, etatFinal.nbPlis)
+  }
 
 
   // initialisation
   // ----------------
 
   // distribue les cartes à travers tous les joueurs
-  private def construitEtatInitial(joueurs: Seq[Joueur]): EtatDuJeu = ???
+  private def construitEtatInitial(joueurs: Seq[Joueur]): EtatDuJeu = {
+    val cartesAPartager = Random.shuffle(Carte.all)
+    val nbCartesParJoueur = cartesAPartager.size / joueurs.size
+    val tas = cartesAPartager.grouped(nbCartesParJoueur).toSeq
+    EtatDuJeu(tasParJoueur = joueurs.zip(tas).toMap, nbPlis = 0)
+  }
+
 
   // Jeu
   // -----------
 
   // Enchaine les plis jusqu'à ce que l'un des joueurs ait toutes les cartes
-  private def jouePlisJusquaVictoire(etatDuJeu: EtatDuJeu): EtatDuJeu = ???
+  @tailrec
+  private def jouePlisJusquaVictoire(etatDuJeu: EtatDuJeu): EtatDuJeu = {
+    logEtatDuJeu(etatDuJeu)
+    if (etatDuJeu.tasParJoueur.size == 1) etatDuJeu
+    else jouePlisJusquaVictoire(jouePli(etatDuJeu))
+  }
 
   // Effectue la logique du prochain pli
   // Retourne l'état du jeu tel qu'il doit se trouver à l'issue du pli
@@ -40,28 +55,55 @@ class BatailleFermee {
   // REGLES : chaque joueur joue la premiere carte de son tas
   // on compare la meilleure carte
   // le joueur qui a la meilleure carte ramasse les cartes qui ont été jouées
-  // et les replace dans son tas
+  // et les replace à la fin de son tas
   //
   // A la fin du pli on remélange le tas de chaque joueur pour contourner
   // le pb des situations qui peuvent tourner en boucle
   //
-  private def jouePli(etatDuJeu: EtatDuJeu): EtatDuJeu = ???
+  private def jouePli(etatDuJeu: EtatDuJeu): EtatDuJeu = {
+    val carteJoueeParJoueur = etatDuJeu.tasParJoueur.mapValues(_.head)
+    val tasRestantParJoueur = etatDuJeu.tasParJoueur.mapValues(_.tail)
+    val joueurGagnant = carteJoueeParJoueur.maxBy(_._2)._1
+    val cartesGagnees = carteJoueeParJoueur.values
+    EtatDuJeu(
+      tasParJoueur =
+        tasRestantParJoueur.map {
+          // pour le gagnant, on lui met les cartes jouees à la fin de son tas
+          case (`joueurGagnant`, tas) => (joueurGagnant, tas ++ cartesGagnees)
+          case other => other
+        }
+        // Virons les joueurs dont le tas est devenu vide
+        .filter(_._2.nonEmpty)
+        // Remélangeons les tas de tout le monde pour éviter des cas qui peuvent tourner en boucle
+        .map { case (joueur, tas) => (joueur, Random.shuffle(tas)) },
+      nbPlis = etatDuJeu.nbPlis + 1
+    )
+  }
 
-  implicit val hauteurOrdering: Ordering[Hauteur] = ???
+  implicit val hauteurOrdering: Ordering[Hauteur] =
+    Ordering.fromLessThan((a, b) => isLessThanUsingSeq(Hauteur.all, a, b))
 
-  implicit val couleurOrdering: Ordering[Couleur] = ???
+  implicit val couleurOrdering: Ordering[Couleur] =
+    Ordering.fromLessThan((a, b) => isLessThanUsingSeq(Couleur.all, a, b))
 
-  implicit val carteOrdering: Ordering[Carte] = ???
+  implicit val carteOrdering: Ordering[Carte] =
+    Ordering.by(carte => (carte.hauteur, carte.couleur))
 
   // En se basant sur une collection 'seq' qui donne l'ordre de référence,
   // cette méthode compare deux éléments a et b et retourne true
   // si a est strictement inférieur à b
-  private def isLessThanUsingSeq[T](seq: Seq[T], a: T, b: T): Boolean = ???
+  private def isLessThanUsingSeq[T](seq: Seq[T], a: T, b: T): Boolean =
+    seq.indexOf(a) < seq.indexOf(b)
 
   // Affiche un résumé de l'état actuel du jeu, pour avoir une idée de où on en est
   // Exemple de ce qui devrait être affiché :
   // Gégé: 10 / Lulu: 31 / Momo: 10
-  private def logEtatDuJeu(etatDuJeu: EtatDuJeu): Unit = ???
+  private def logEtatDuJeu(etatDuJeu: EtatDuJeu): Unit =
+    println {
+      etatDuJeu.tasParJoueur.toSeq.sortBy(_._1.nom).map { case (joueur, tas) =>
+        s"${joueur.nom}: ${tas.size}"
+      }.mkString(" / ")
+    }
 
 
 }
