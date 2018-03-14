09ec116bb7522cad3d28d34c7be2307df510a5ad
implicitdef <implicitdef@users.noreply.github.com>
2018-03-14 18:15:14 +0100

    working on futures exo

--- corrections/exo_futures/src/main/scala/Main.scala
+++ corrections/exo_futures/src/main/scala/Main.scala
@@ -8,12 +8,29 @@ object Main {
   // A implémenter en se basant sur les méthodes de Api
   // (utiliser des flatMap, pas de for-expression)
   def getTextsOfPostsCommentedByUser(userId: Int): Future[Seq[String]] =
-    ???
+    api.getCommentsByUserId(userId).flatMap { userComments =>
+      api.getAllPosts.map { posts =>
+        val userCommentsIds = userComments.map(_.id)
+        val postsCommentedByUser = posts.filter { post =>
+          userCommentsIds.contains(post.id)
+        }
+        postsCommentedByUser.map(_.text)
+      }
+    }
 
   // A implémenter en se basant sur les méthodes de Api
   // (utiliser des for-expression, pas de flatMap)
   def getTextsOfPostsCommentedByUserWithForExpressions(userId: Int): Future[Seq[String]] =
-    ???
+    for {
+      userComments <- api.getCommentsByUserId(userId)
+      posts <- api.getAllPosts
+    } yield {
+      val userCommentsIds = userComments.map(_.id)
+      val postsCommentedByUser = posts.filter { post =>
+        userCommentsIds.contains(post.id)
+      }
+      postsCommentedByUser.map(_.text)
+    }
 
   def main(args: Array[String]): Unit = {
     val texts = Await.result(getTextsOfPostsCommentedByUser(1), 10.seconds)
