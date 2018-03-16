fb8a93194d51493719f09b07fcf0bf40af69b4cc
implicitdef <implicitdef@users.noreply.github.com>
2018-03-16 15:32:50 +0100

    12 days of christmas

---
+++ corrections/exo_12_days_of_christmas/src/main/scala/example/Hello.scala
@@ -0,0 +1,106 @@
+package example
+
+object Hello extends App {
+
+
+
+  // Afficher les paroles de la comptine "Twelves days of christmas"
+  // https://en.wikipedia.org/wiki/The_Twelve_Days_of_Christmas_(song)
+  // Exemple :
+  /*
+
+  On the First day of Christmas my true love sent to me
+  a Partridge in a Pear Tree.
+
+  On the Second day of Christmas my true love sent to me
+  Two Turtle Doves
+  and a Partridge in a Pear Tree.
+
+  On the Third day of Christmas my true love sent to me
+  Three French Hens
+  Two Turtle Doves
+  and a Partridge in a Pear Tree.
+
+  (...)
+
+  On the Twelfth day of Christmas my true love sent to me
+  A Partridge in a Pear Tree
+  Two Turtle Doves
+  Three French Hens
+  Four Calling Birds
+  Five Gold Rings
+  Six Geese a-Laying
+  Seven Swans a-Swimming
+  Eight Maids a-Milking
+  Nine Ladies Dancing
+  Ten Lords a-Leaping
+  Eleven Pipers Piping
+  And twelve Drummers Drumming
+
+   */
+  def twelveDaysOfChristmas(): Unit = {
+    days.foreach { day =>
+      println(couplet(day))
+      println()
+    }
+  }
+
+  // Donne la String finale pour un couplet donné
+  def couplet(day: Day): String = {
+    val gifts = allGifts(day)
+    val previousGifts = gifts.init
+    val todayGift = gifts.last
+    (
+      firstLine(day) +: (
+        if (previousGifts.isEmpty)
+          Seq(todayGift)
+        else
+          previousGifts :+ ("And " + todayGift)
+      )
+    ).map(_.capitalize).mkString("\n")
+  }
+
+  // Donne la première ligne du couplet ("On the ... sent to me")
+  def firstLine(day: Day): String =
+    s"On the ${day.numberAdjective} day of Christmas my true love sent to me"
+
+  // Liste (dans l'ordre) tous les cadeaux donnés pour un certain jour
+  def allGifts(day: Day): Seq[String] = {
+    getPreviousDay(day) match {
+      case None => Seq(day.gift)
+      case Some(previous) => allGifts(previous) :+ day.gift
+    }
+  }
+
+  // Indique quel jour était le précédent de celui passé en paramètre
+  // (ou None si c'était le premier jour)
+  def getPreviousDay(day: Day): Option[Day] = {
+    days.sliding(2).collectFirst {
+      case Seq(previous, `day`) => previous
+    }
+  }
+
+  case class Day(
+    numberAdjective: String,
+    gift: String
+  )
+
+  val days = Seq(
+    Day("First", "a Partridge in a Pear Tree"),
+    Day("Second", "two Turtle Doves"),
+    Day("Third", "three French Hens"),
+    Day("Fourth", "four Calling Birds"),
+    Day("Fifth", "five Gold Rings"),
+    Day("Sixth", "six Geese a-Laying"),
+    Day("Seventh", "seven Swans a-Swimming"),
+    Day("Eighth", "eight Maids a-Milking"),
+    Day("Ninth", "nine Ladies Dancing"),
+    Day("Tenth", "ten Lords a-Leaping"),
+    Day("Eleventh", "eleven Pipers Piping"),
+    Day("Twelfth", "twelve Drummers Drumming")
+  )
+
+
+  twelveDaysOfChristmas()
+
+}
